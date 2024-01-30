693c848de9a04b90b2826b8635c8db733d78d5fe
FADHILI Josue <FADHILI Josue>
2024-01-30 14:29:44 +0200

    fix: carousel

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<HomePage />} />
       <Route path="/*" element={<div className='w-screen min-h-[70vh] flex flex-col items-center justify-center text-4xl'><h1>Not Found</h1></div>} />
       </Routes>
     </BrowserRouter>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { MdWbSunny } from 'react-icons/md'
 import { useData } from '../providers/Providers'
 import { IoMenu } from "react-icons/io5";
@@ -10,13 +10,24 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "../components/ui/Sheet"
+import { useLocation } from 'react-router-dom';
+import { cn } from '../lib/utils';
 
 const Navbar: FC = () => {
   const { toggleMode } = useData()
+  
+  const router = useLocation()
+  const [active, setActive] = useState<string>('');
+
+  useEffect(() => {
+      const { pathname } = router;
+      const path = pathname.split("/")[1];
+          setActive(path)
+  }, [router]);
 
   return <div className="flex w-full justify-end md:justify-between my-16 text-zinc-700 font-semibold dark:text-zinc-400">
-    <p className='hidden md:inline-block '>ARTIST STATEMENT</p>
-    <p className='hidden md:inline-block '>WORKS</p>
+    <p className={cn('hidden md:inline-block', {'bg-gradient-to-r p-0 from-dark-gold via-light-gold to-black/30 bg-[length:100%_2px] bg-no-repeat bg-bottom': active === "/about"})}>ARTIST STATEMENT</p>
+    <p className='hidden md:inline-block'>WORKS</p>
     <h1 className='hidden md:inline-block font-bold text-3xl font-serif'>Madoc Pierce</h1>
     <p className='hidden md:inline-block '>CONTACT</p>
     <button className='hidden md:flex bg-stone-300/40 h-8 w-8 items-center justify-center rounded-full' onClick={() => toggleMode()}><MdWbSunny size={20} className='text-black dark:text-white' /></button>
