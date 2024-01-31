9c95794cbd44159a3ee20a967787ba30b632df2c
FADHILI Josue <FADHILI Josue>
2024-01-31 22:42:48 +0200

    fix: images exts

---
+++ src/index.d.ts
@@ -0,0 +1,2 @@
+declare module '*.JPG';
+declare module '*.PNG';
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ["**/*.jpg", "**/*.png", "**/*.JPG", "**/*.PNG", "**/*.heic"],
   plugins: [react()],
 })
