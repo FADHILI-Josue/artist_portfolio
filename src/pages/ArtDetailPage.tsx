44f5084537aa5c193eb48bb3a9cbc49e23f51833
FADHILI Josue <FADHILI Josue>
2024-02-01 20:07:40 +0200

    fix: art detail page

--- src/pages/ArtDetailPage.tsx
+++ src/pages/ArtDetailPage.tsx
@@ -38,18 +38,18 @@ const ArtDetailPage = () => {
                         <button className='text-light-gold hover:scale-110 transition-all duration-300' onClick={() => navigate(-1)}>&larr; Back</button>
                         <button className='flex bg-stone-300/40 h-8 w-8 items-center justify-center rounded-full' onClick={() => toggleMode()}><MdWbSunny size={20} className='text-black dark:text-white hover:scale-110 transition-all duration-200' /></button>
                     </div>
-                    <div className="w-full flex flex-col sm:flex-row bg-[#281c0c]/30">
+                    <div className="w-full flex flex-col md:flex-row bg-stone-100/50 dark:bg-[#281c0c]/30">
                         <div className="flex-1">
                             <img src={arts[Number(id)]?.image} alt="contact banner" className='w-full object-contain' />
                         </div>
                         <div className="flex-1 text-sm sm:text-base py-10 sm:px-10 lg:px-20 space-y-3 sm:space-y-6">
-                            <div className="flex px-2 sm:px-0 items-center space-x-5 sm:space-x-10"><span className='text-stone-300'>Title:</span> <span className='font-medium sm:font-bold'>{arts[Number(id)]?.title}</span></div>
-                            <div className="flex px-2 sm:px-0 items-center space-x-5 sm:space-x-10"><span className='text-stone-300'>Size:</span> <span className='font-medium sm:font-bold'>{arts[Number(id)]?.size}</span></div>
-                            <div className="flex px-2 sm:px-0 items-center space-x-5 sm:space-x-10"><span className='text-stone-300'>Medium:</span> <span className='font-medium sm:font-bold'>{arts[Number(id)]?.medium}</span></div>
-                            <div className="rounded-md bg-stone-800/50 px-6 pt-4 pb-7">
+                            <div className="flex px-2 sm:px-0 items-center space-x-5 sm:space-x-10"><span className='dark:text-stone-300'>Title:</span> <span className='indent-8 font-medium sm:font-bold'>{arts[Number(id)]?.title}</span></div>
+                            <div className="flex px-2 sm:px-0 items-center space-x-5 sm:space-x-10"><span className='dark:text-stone-300'>Size:</span> <span className='indent-8 font-medium sm:font-bold'>{arts[Number(id)]?.size}</span></div>
+                            <div className="flex px-2 sm:px-0 items-center space-x-5 sm:space-x-10"><span className='dark:text-stone-300'>Medium:</span> <span className='font-medium sm:font-bold'>{arts[Number(id)]?.medium}</span></div>
+                            <div className="rounded-md bg-black bg-opacity-[3%] dark:bg-stone-800/50 px-6 pt-4 pb-7">
                                 <h1 className='border-b border-b-stone-400 py-2 font-semibold'>Inquire about availability:</h1>
                                 <form className='space-y-7 py-10'>
-                                    <Input label='Email' placeholder='example@gmail.com' />
+                                    <Input label='Email' className='bg-black bg-opacity-5 placeholder-stone-800 dark:placeholder-stone-500 dark:bg-black/20 focus:bg-stone-500/10 dark:focus:bg-white/10 border-b-stone-700 dark:border-stone-300' placeholder='example@gmail.com' />
                                     <button className='active:scale-95 mb-10 sm:mb-0 cursor-pointer inline-flex items-center justify-center rounded-md text-sm transition-colors bg-light-gold text-black hover:bg-dark-gold h-10 py-2 px-4 divide-x [&>*]:px-3 font-semibold divide-black w-full'><span className='w-full'>Send Message</span><span className='text-xl'>&rarr;</span></button>
                                 </form>
                             </div>
